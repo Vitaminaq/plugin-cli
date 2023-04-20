e6b40bec214008724e28081fda1e22516a852564
肖鹏飞 <xiaopf@300624.cn>
2023-04-20 11:30:51 +0800

    chore: 删除无用代码

--- packages/cli/lib/webpack/config/build.ts
+++
@@ -1,9 +0,0 @@
-import { WebpackBaseConfig } from "./base";
-
-export class WebpackBuildConfig extends WebpackBaseConfig {
-    public constructor() {
-        super();
-        this.config.mode("production").devtool(false);
-        this.config.optimization.usedExports(false);
-    }
-}
--- packages/cli/lib/webpack/config/dev.ts
+++
@@ -1,9 +0,0 @@
-import { WebpackBaseConfig } from "./base";
-
-export class WebpackDevConfig extends WebpackBaseConfig {
-    public constructor() {
-        super();
-        this.config.mode("production").devtool(false);
-        this.config.optimization.sideEffects(false).usedExports(false);
-    }
-}
